0214df725fcc9a3fd702365e9ccaee6ccc7c7be4
Brijesh Kushawaha <v-brkus@microsoft.com>
2019-11-15 11:17:37 +0530

    initial changes

---
+++ build/precache-manifest.fc1696cf1327dccd186a425c96e8c33b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e369884f956f3e8d888836f6dfaa0e9",
+    "url": "/react-Todo/index.html"
+  },
+  {
+    "revision": "e566692f5955d490c501",
+    "url": "/react-Todo/static/css/main.1fbf81d8.chunk.css"
+  },
+  {
+    "revision": "ce5f49ef327aab0806b7",
+    "url": "/react-Todo/static/js/2.c86f4eef.chunk.js"
+  },
+  {
+    "revision": "e566692f5955d490c501",
+    "url": "/react-Todo/static/js/main.813800d5.chunk.js"
+  },
+  {
+    "revision": "376d0a6e7f70660cb2a1",
+    "url": "/react-Todo/static/js/runtime~main.70ce81f2.js"
+  }
+]);
